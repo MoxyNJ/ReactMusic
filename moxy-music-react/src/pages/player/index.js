2f6d179de4b80f8789471015ce799ab868266424
moxy7 <754184966@qq.com>
2021-10-31 22:47:10 +0800

    更新2

---
+++ moxy-music-react/src/pages/player/index.js
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+
+import { PlayerWrapper, PlayerLeft, PlayerRight } from "./style";
+
+export default memo(function LJPlayer() {
+  return <PlayerWrapper>PlayerWrapper</PlayerWrapper>;
+});
