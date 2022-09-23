e7971bdb18b4f74071a5d92daf05ea92e2dac642
ninjee <moxy7@qq.com>
2022-09-23 18:00:30 +0800

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,9 @@
-// const devBaseURL = "https://autumnfish.cn";
-// const proBaseURL = "https://autumnfish.cn";
+const devBaseURL = "https://autumnfish.cn";
+const proBaseURL = "https://autumnfish.cn";
 // const devBaseURL = "https://musicapi-opal.vercel.app";
 // const proBaseURL = "https://musicapi-opal.vercel.app";
-const devBaseURL = "http://1.116.149.28:3000";
-const proBaseURL = "http://1.116.149.28:3000";
+// const devBaseURL = "http://1.116.149.28:3000";
+// const proBaseURL = "http://1.116.149.28:3000";
 
 
 export const BASE_URL =
