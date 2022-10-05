17983cc41907b4e4cd17e43c17ef9debac89f919
ninjee <moxy7@qq.com>
2022-10-05 12:12:53 +0800

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,11 @@
-const devBaseURL = "https://autumnfish.cn";
-const proBaseURL = "https://autumnfish.cn";
+// const devBaseURL = "https://autumnfish.cn";
+// const proBaseURL = "https://autumnfish.cn";
 // const devBaseURL = "https://musicapi-opal.vercel.app";
 // const proBaseURL = "https://musicapi-opal.vercel.app";
 // const devBaseURL = "http://1.116.149.28:3000";
 // const proBaseURL = "http://1.116.149.28:3000";
+const devBaseURL = "https://musicapi.ninjee.top/";
+const proBaseURL = "https://musicapi.ninjee.top/";
 
 
 export const BASE_URL =
